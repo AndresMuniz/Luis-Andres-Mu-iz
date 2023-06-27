c8cf810d728de014c5eeeb35cc7cf3bb3d409e13
AndresMuniz <lmunis-2019127@kinal.edu.gt>
2023-06-27 15:26:23 -0600

    Update Proyecto_final_de_bimestre_lll.ino

--- 3er_bimestre/Proyecto_final_de_bimestre_lll/Proyecto_final_de_bimestre_lll.ino
+++ 3er_bimestre/Proyecto_final_de_bimestre_lll/Proyecto_final_de_bimestre_lll.ino
@@ -11,6 +11,7 @@ SoftwareSerial bluetooth(0,1);
 Servo servo1;
 Servo servo2;
 Servo servo3;
+Servo servo4;
 
 void setup() {
   Serial.begin(9600);//iniciamos la comunicacion con la consola
@@ -18,6 +19,7 @@ void setup() {
   servo1.attach(3);//le asignamos un pin al servo 1
   servo2.attach(5);//le asignamos un pin al servo 2
   servo3.attach(6);//le asignamos un pin al servo 3
+  servo3.attach(9);//le asignamos un pin al servo 4
 }
 
 void loop() {
@@ -29,7 +31,8 @@ void fun_bluetooth (){
   unsigned int servopos1 = bluetooth.read();
   unsigned int servopos2 = bluetooth.read();
   unsigned int servopos3 = bluetooth.read();
-  unsigned int realservo = (servopos2 *256)  + servopos1 + servopos3;
+  unsigned int servopos4 = bluetooth.read();
+  unsigned int realservo = (servopos2 *256)  + servopos1 + servopos3 + servopos4;
   Serial.println(realservo);
 
   if (realservo >= 1000 && realservo < 1180){
@@ -41,15 +44,22 @@ void fun_bluetooth (){
 
   if (realservo >= 2000 && realservo < 2180){
     int servomap2 = realservo;
-    servomap2 = map(servomap2, 1000,1180,0,180);
+    servomap2 = map(servomap2, 2000,2180,0,180);
     servo2.write(servomap2);
     Serial.println("Servo 2 ON"); 
   }
 
   if (realservo >= 3000 && realservo < 3180){
     int servomap3 = realservo;
-    servomap3 = map(servomap3, 1000,1180,0,180);
+    servomap3 = map(servomap3, 3000,3180,0,180);
     servo3.write(servomap3);
     Serial.println("Servo 3 ON"); 
+  }
+
+  if (realservo >= 4000 && realservo < 4180){
+    int servomap4 = realservo;
+    servomap4 = map(servomap4, 4000,4180,0,180);
+    servo3.write(servomap4);
+    Serial.println("Servo 4 ON"); 
   }
 }
